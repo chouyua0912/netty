25bd9bf41be1b10e2099d723ded8023af5e9b38e
chouyua <logsoi@126.com>
2018-10-16 23:41:01 +0800

    modify

---
+++ src/main/java/z/learn/discardserver/DiscardServerHandler.java
@@ -0,0 +1,26 @@
+package z.learn.discardserver;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ * 覆盖读取到消息的事件
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)       必须要释放掉外部缓存
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/z/learn/discardserver/package-info.java
@@ -0,0 +1,7 @@
+package z.learn.discardserver;
+
+/**
+ * 事件驱动模型
+ * 网络操作核心由netty实现
+ * 业务代码只要处理相应的事件
+ */
