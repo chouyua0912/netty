1a7c0c3296fb9a6ce76538b83e80f1997c53d19a
chouyua <logsoi@126.com>
2018-10-20 22:18:13 +0800

    modify

---
+++ src/main/java/z/learn/httpclient/HttpClient.java
@@ -0,0 +1,66 @@
+package z.learn.httpclient;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.*;
+
+import java.net.URI;
+
+/**
+ * http://hc.apache.org/httpcomponents-core-ga/examples.html
+ * http://book.51cto.com/art/201406/443921.htm
+ */
+public class HttpClient {
+
+    public void run(String host, int port) throws Exception {
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(workerGroup);
+            bootstrap.channel(NioSocketChannel.class)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            // 客户端接收到的是httpResponse响应，所以要使用HttpResponseDecoder进行解码
+                            ch.pipeline().addLast(new HttpResponseDecoder());
+                            // 客户端发送的是httprequest，所以要使用HttpRequestEncoder进行编码
+                            ch.pipeline().addLast(new HttpRequestEncoder());
+                            ch.pipeline().addLast(new HttpClientInboundHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+
+            URI uri = new URI("http://" + host + ":" + port);
+            String msg = "Are you ok?";
+            DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,
+                    uri.toASCIIString(), Unpooled.wrappedBuffer(msg.getBytes("UTF-8")));
+
+            // 构建http请求
+            request.headers().set(HttpHeaders.Names.HOST, host);
+            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, request.content().readableBytes());
+            // 发送http请求
+            future.channel().write(request);
+            future.channel().flush();
+            future.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        HttpClient client = new HttpClient();
+        client.run("127.0.0.1", 8080);
+    }
+}
---
+++ src/main/java/z/learn/httpclient/HttpClientInboundHandler.java
@@ -0,0 +1,50 @@
+package z.learn.httpclient;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+
+public class HttpClientInboundHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpResponse) {
+            HttpResponse response = (HttpResponse) msg;
+            System.out.println("CONTENT_TYPE:" + response.headers().get(HttpHeaders.Names.CONTENT_TYPE));
+        }
+        if (msg instanceof HttpContent) {
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
+/**
+ * channelRead:24, HttpClientInboundHandler (z.learn.httpclient)
+ * invokeChannelRead:362, AbstractChannelHandlerContext (io.netty.channel)
+ * invokeChannelRead:348, AbstractChannelHandlerContext (io.netty.channel)
+ * fireChannelRead:340, AbstractChannelHandlerContext (io.netty.channel)
+ * fireChannelRead:310, ByteToMessageDecoder (io.netty.handler.codec)
+ * fireChannelRead:297, ByteToMessageDecoder (io.netty.handler.codec)
+ * callDecode:413, ByteToMessageDecoder (io.netty.handler.codec)
+ * channelRead:265, ByteToMessageDecoder (io.netty.handler.codec)
+ * invokeChannelRead:362, AbstractChannelHandlerContext (io.netty.channel)
+ * invokeChannelRead:348, AbstractChannelHandlerContext (io.netty.channel)
+ * fireChannelRead:340, AbstractChannelHandlerContext (io.netty.channel)
+ * channelRead:1434, DefaultChannelPipeline$HeadContext (io.netty.channel)
+ * invokeChannelRead:362, AbstractChannelHandlerContext (io.netty.channel)
+ * invokeChannelRead:348, AbstractChannelHandlerContext (io.netty.channel)
+ * fireChannelRead:965, DefaultChannelPipeline (io.netty.channel)
+ * read:163, AbstractNioByteChannel$NioByteUnsafe (io.netty.channel.nio)
+ * processSelectedKey:645, NioEventLoop (io.netty.channel.nio)
+ * processSelectedKeysOptimized:580, NioEventLoop (io.netty.channel.nio)
+ * processSelectedKeys:497, NioEventLoop (io.netty.channel.nio)
+ * run:459, NioEventLoop (io.netty.channel.nio)
+ * run:884, SingleThreadEventExecutor$5 (io.netty.util.concurrent)
+ * run:30, FastThreadLocalRunnable (io.netty.util.concurrent)
+ * run:748, Thread (java.lang)
+ */
